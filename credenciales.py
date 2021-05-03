9bd62ec3d01db32e65ef9833dece5c6a545a2ce8
Isma <jhonnygarcia201830454@cunoc.edu.gt>
2021-05-02 20:50:56 -0600

    first

---
+++ credenciales.py
@@ -0,0 +1,2 @@
+user_="ismaelherz2001@gmail.com"
+password_="hernandezGarcia2001"
---
+++ send_email.py
@@ -0,0 +1,73 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import credenciales
+
+
+# https://www.hostinger.es/tutoriales/como-usar-el-servidor-smtp-gmail-gratuito/
+
+correo_user=credenciales.user_
+password_user=credenciales.password_
+
+def enviar_msj():
+    global correo_user
+    global password_user
+    remitente = correo_user
+    destinatarios = [correo_user]
+    asunto = 'fichero'
+    cuerpo = 'msj'
+    ruta_adjunto = '/home/jhonny/Documentos/Python/reconocimiento_facial/file.bin'
+    nombre_adjunto = 'file.bin'
+
+    # Creamos el objeto mensaje
+    mensaje = MIMEMultipart()
+
+    # Establecemos los atributos del mensaje
+    mensaje['From'] = remitente
+    mensaje['To'] = ", ".join(destinatarios)
+    mensaje['Subject'] = asunto
+
+    # Agregamos el cuerpo del mensaje como objeto MIME de tipo texto
+    mensaje.attach(MIMEText(cuerpo, 'plain'))
+
+    # Abrimos el archivo que vamos a adjuntar
+    archivo_adjunto = open(ruta_adjunto, 'rb')
+
+    # Creamos un objeto MIME base
+    adjunto_MIME = MIMEBase('application', 'octet-stream')
+    # Y le cargamos el archivo adjunto
+    adjunto_MIME.set_payload((archivo_adjunto).read())
+    # Codificamos el objeto en BASE64
+    encoders.encode_base64(adjunto_MIME)
+    # Agregamos una cabecera al objeto
+    adjunto_MIME.add_header('Content-Disposition',
+                            "attachment; filename= %s" % nombre_adjunto)
+    # Y finalmente lo agregamos al mensaje
+    mensaje.attach(adjunto_MIME)
+
+    # Creamos la conexión con el servidor
+    sesion_smtp = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # Ciframos la conexión
+    sesion_smtp.starttls()
+
+    # Iniciamos sesión en el servidor
+    sesion_smtp.login(correo_user, password_user)
+
+    # Convertimos el objeto mensaje a texto
+    texto = mensaje.as_string()
+
+    # Enviamos el mensaje
+    sesion_smtp.sendmail(remitente, destinatarios, texto)
+
+    # Cerramos la conexión
+    sesion_smtp.quit()
+
+
+
+
+
+enviar_msj()
+
